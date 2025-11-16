78cbd71f2ecffb4f313adb3d6c415d9ec54fcb03
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-11-16 20:26:26 +0300

    ...

--- tasks/trofimov_n_max_val_matrix/common/include/common.hpp
+++ tasks/trofimov_n_max_val_matrix/common/include/common.hpp
@@ -10,7 +10,7 @@ namespace trofimov_n_max_val_matrix {
 
 using InType = std::vector<std::vector<int>>;
 using OutType = std::vector<int>;
-using TestType = std::tuple<int, std::string>;
+using TestType = std::tuple<InType, OutType>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace trofimov_n_max_val_matrix
--- tasks/trofimov_n_max_val_matrix/tests/functional/main.cpp
+++ tasks/trofimov_n_max_val_matrix/tests/functional/main.cpp
@@ -1,55 +1,71 @@
 #include <gtest/gtest.h>
+#include <mpi.h>
 
-#include <algorithm>
+#include <array>
 #include <cstddef>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "trofimov_n_max_val_matrix/common/include/common.hpp"
+#include "trofimov_n_max_val_matrix/mpi/include/ops_mpi.hpp"
+#include "trofimov_n_max_val_matrix/seq/include/ops_seq.hpp"
 #include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace trofimov_n_max_val_matrix {
 
 class TrofimovNRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
-    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
+    const auto &input_matrix = std::get<0>(test_param);
+    const auto &expected_output = std::get<1>(test_param);
+
+    std::string test_name = "matrix_" + std::to_string(input_matrix.size()) + "x" +
+                            (input_matrix.empty() ? "0" : std::to_string(input_matrix[0].size()));
+    if (!expected_output.empty()) {
+      test_name += "_maxes";
+      for (size_t i = 0; i < expected_output.size(); ++i) {
+        test_name += "_" + FormatNumber(expected_output[i]);
+      }
+    }
+
+    return test_name;
+  }
+
+ private:
+  static std::string FormatNumber(int num) {
+    if (num >= 0) {
+      return std::to_string(num);
+    }
+    return "neg" + std::to_string(-num);
   }
 
  protected:
   void SetUp() override {
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    int matrix_size = std::get<0>(params);
-
-    input_data_.clear();
-    expected_output_.clear();
-
-    input_data_.reserve(static_cast<std::size_t>(matrix_size));
-    expected_output_.reserve(static_cast<std::size_t>(matrix_size));
-
-    for (int i = 0; i < matrix_size; ++i) {
-      std::vector<int> row;
-      row.reserve(static_cast<std::size_t>(matrix_size));
-
-      for (int j = 0; j < matrix_size; ++j) {
-        row.push_back((i * matrix_size) + j);
-      }
-      input_data_.push_back(row);
-
-      expected_output_.push_back(*std::ranges::max_element(row));
-    }
+    input_data_ = std::get<0>(params);
+    expected_output_ = std::get<1>(params);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
+    int world_rank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if (world_rank != 0) {
+      return true;
+    }
+
     if (output_data.size() != expected_output_.size()) {
       return false;
     }
 
-    for (std::size_t i = 0; i < expected_output_.size(); ++i) {
+    for (size_t i = 0; i < output_data.size(); ++i) {
       if (output_data[i] != expected_output_[i]) {
         return false;
       }
     }
+
     return true;
   }
 
@@ -61,5 +77,37 @@ class TrofimovNRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType
   InType input_data_;
   OutType expected_output_;
 };
+
+namespace {
+
+TEST_P(TrofimovNRunFuncTestsProcesses, FindMaxInEachRow) {
+  ExecuteTest(GetParam());
+}
+
+const std::array<TestType, 6> kTestParam = {
+    std::make_tuple(std::vector<std::vector<int>>{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, std::vector<int>{3, 6, 9}),
+
+    std::make_tuple(std::vector<std::vector<int>>{{-1, -5, -3}, {-9, -2, -7}}, std::vector<int>{-1, -2}),
+
+    std::make_tuple(std::vector<std::vector<int>>{{5, 8, 2, 10, 1}}, std::vector<int>{10}),
+
+    std::make_tuple(std::vector<std::vector<int>>{{7, 7, 7}, {7, 7, 7}}, std::vector<int>{7, 7}),
+
+    std::make_tuple(std::vector<std::vector<int>>{{1, 5, 1}, {3, 3, 4}}, std::vector<int>{5, 4}),
+
+    std::make_tuple(std::vector<std::vector<int>>{{42}}, std::vector<int>{42})};
+
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<TrofimovNMaxValMatrixMPI, InType>(kTestParam, PPC_SETTINGS_trofimov_n_max_val_matrix),
+    ppc::util::AddFuncTask<TrofimovNMaxValMatrixSEQ, InType>(kTestParam, PPC_SETTINGS_trofimov_n_max_val_matrix)
+);
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kPerfTestName = TrofimovNRunFuncTestsProcesses::PrintFuncTestName<TrofimovNRunFuncTestsProcesses>;
+
+INSTANTIATE_TEST_SUITE_P(MatrixMaxTests, TrofimovNRunFuncTestsProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace
 
 }  // namespace trofimov_n_max_val_matrix
